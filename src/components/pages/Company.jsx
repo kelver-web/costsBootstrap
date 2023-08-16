79646c3deec5f2beabfb98592660c9b6add3804b
kelver-web <kelverwt@gmail.com>
2023-08-16 13:12:15 -0300

    contact form

--- src/components/pages/Company.jsx
+++ src/components/pages/Company.jsx
@@ -1,7 +1,36 @@
-
+import Container from 'react-bootstrap/Container'
 
 function Company() {
-  return <h1>Página da empresa</h1>
+  return (
+    <Container className='px-4 text-justify'>
+      <Container className="d-flex justify-content-sm-between align-items-sm-center my-5 px-4">
+        <Container className="">
+          <h1 className='d-flex text-warning fw-bolder'>Sobre a empresa:</h1>
+          <p className='text-warning fw-bolder'>Nome da Empresa: <span className='fw-light'>Lorem ipsum dolor sit amet.</span></p>
+          <p className='text-warning fw-bolder'>Descrição: <br /><span className='text-warning fw-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum non iusto minima qui facere numquam et porro facilis sequi ab vero, saepe quidem aliquam rerum exercitationem eligendi nam cupiditate veritatis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae odit commodi et nostrum, nihil corporis ab architecto eaque eligendi consequuntur expedita laboriosam culpa facilis harum beatae veniam magnam. Voluptate, vitae?</span></p>
+
+          <p className='text-warning fw-bolder'>Nossos Serviços: <br /><span className='text-warning fw-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum non iusto minima qui facere numquam et porro facilis sequi ab vero, saepe quidem aliquam rerum exercitationem eligendi nam cupiditate veritatis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae odit commodi et nostrum, nihil corporis ab architecto eaque eligendi consequuntur expedita laboriosam culpa facilis harum beatae veniam magnam. Voluptate, vitae?</span></p>
+
+          <p className='text-warning fw-bolder'>Missão: <br /><span className='text-warning fw-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum non iusto minima qui facere numquam et porro facilis sequi ab vero, saepe quidem aliquam rerum exercitationem eligendi nam cupiditate veritatis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae odit commodi et nostrum, nihil corporis ab architecto eaque eligendi consequuntur expedita laboriosam culpa facilis harum beatae veniam magnam. Voluptate, vitae? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos ipsum eligendi itaque est nam, amet alias beatae impedit ipsam dolore vitae repudiandae eum, at, cum delectus doloribus quaerat maxime odit.</span></p>
+
+          <p className='text-warning fw-bolder'>Valores: <br />
+            <ul className='fw-light px-3'>
+              <li>Inovação: Buscamos constantemente novas abordagens e soluções para impulsionar o progresso tecnológico.</li>
+              <li>Integridade: Agimos com honestidade, ética e transparência em todas as interações.</li>
+              <li>Colaboração: Trabalhamos em estreita colaboração com nossos clientes para alcançar objetivos comuns.</li>
+              <li>Excelência: Buscamos a excelência em tudo o que fazemos, buscando constantemente a melhoria.</li>
+            </ul>
+          </p>
+          
+          <p className='text-warning'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia incidunt eius numquam corporis veniam eveniet ullam, quaerat illum consectetur. Voluptatum consequatur modi nobis sed dignissimos, soluta nostrum! Dolorum, doloremque natus!</p>
+          
+        </Container>
+      </Container>
+      <Container>
+        
+      </Container>
+    </Container>
+  )
 }
 
 export default Company
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,6 +1,83 @@
+import Container from "react-bootstrap/Container"
 
 function Contact() {
-  return <h1>Página de contato</h1>
+  return (
+
+    <section className="mb-4">
+      <h2 className="h1-responsive fw-bolder text-center my-4 text-warning">Contato</h2>
+
+      <Container className="row">
+
+        <Container className="d-flex justify-content-center align-items-center">
+          <Container className="col-md-9 mb-md-0 mb-5 ">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+            <Container className="row">
+              <Container className="col-md-12">
+                <Container className="md-form mt-4">
+                  <p className="text-center w-responsive mx-auto mb-5 text-warning">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+                    a matter of hours to help you.</p>
+                </Container>
+              </Container>
+            </Container>
+              <Container className="row">
+                <Container className="col-md-6">
+                  <Container className="md-form mb-0">
+                    <label htmlFor="name" className="text-white">Seu nome:</label>
+                    <input type="text" id="name" name="name" className="form-control" placeholder="Nome"/>
+                  </Container>
+                </Container>
+                <Container className="col-md-6">
+                  <Container className="md-form mb-0">
+                    <label htmlFor="email" className="text-white">Seu melhor e-mail:</label>
+                    <input type="text" id="email" name="email" className="form-control" placeholder="E-mail"/>
+                  </Container>
+                </Container>
+              </Container>
+              <Container className="row">
+                <Container className="col-md-12">
+                  <Container className="md-form mt-4">
+                    <label htmlFor="subject" className="text-white">Assunto:</label>
+                    <input type="text" id="subject" name="subject" className="form-control" placeholder="Assunto"/>
+                  </Container>
+                </Container>
+              </Container>
+              <Container className="row">
+                <Container className="col-md-12">
+                  <Container className="md-form mt-4">
+                    <label htmlFor="message" className="text-white">Escreva a sua mensagem:</label>
+                    <textarea type="text" id="message" name="message" rows="2" className="form-control md-textarea"></textarea>
+                  </Container>
+                </Container>
+              </Container>
+            </form>
+            <Container className="text-md-left mt-4 px-4">
+              <a type="button" className="btn btn-outline-warning px-5">Enviar</a>
+            </Container>
+            <Container className="status"></Container>
+          </Container>
+        </Container>
+
+        <Container className="col-md-3 text-center">
+          <ul className="list-unstyled mb-0">
+            <li><i className="fas fa-map-marker-alt fa-2x"></i>
+              <p>San Francisco, CA 94126, USA</p>
+            </li>
+
+            <li><i className="fas fa-phone mt-4 fa-2x"></i>
+              <p>+ 01 234 567 89</p>
+            </li>
+
+            <li><i className="fas fa-envelope mt-4 fa-2x"></i>
+              <p>contact@mdbootstrap.com</p>
+            </li>
+          </ul>
+        </Container>
+
+      </Container>
+
+    </section>
+
+  )
 }
 
 export default Contact
